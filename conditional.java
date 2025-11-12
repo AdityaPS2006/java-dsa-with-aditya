eec3c49f2544735ef5584247212dc99fb377eb4e
Aditya Pratap Singh <adps.prayag@gmail.com>
2025-11-12 22:44:39 +0530

    conditional statement question of java

---
+++ conditional.java
@@ -0,0 +1,100 @@
+import java.util.*;
+public class conditional {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        // int age=sc.nextInt();
+        // if(age>=18){
+        //     System.out.println("adult");
+        // }if(age<18 && age>13){
+        //     System.out.println("teenager");
+        // }else{
+        //     System.out.println("child");
+        // }
+
+
+
+    
+        // int age=sc.nextInt();
+        // if(age>=18){
+        //     System.out.println("adult");
+        // }else if(age>13 && age<18){
+        //     System.out.println("teenager");
+        // }else{
+        //     System.out.println("child");
+        // }
+
+
+
+
+
+        // int larger=(5>3)?5:3;
+        // System.out.println(larger);
+
+        // int larger=(5<3)?5:3;
+        // System.out.println(larger);
+
+        // String type=(5%2==0)?"even":"odd";
+        // System.out.println("odd");
+
+
+        
+        // int n=sc.nextInt();
+        // switch(n){
+        //     case 1:System.out.println("Samosa");
+        //     break;
+        //     case 2:System.out.println("Burger");
+        //     break;
+        //     case 3:System.out.println("momo");
+        //     break;
+        //     default:System.out.println("wake up");
+        // }
+
+        
+
+
+        //PRATICE QUESTIONS
+        // int n=sc.nextInt();
+        // if(n>0){
+        //     System.out.println("positive number");
+        // }else{
+        //     System.out.println("negative number");
+        // }
+
+
+
+        // double temp=100.1   ;
+        // if(temp>100){
+        //     System.out.println("you have a fever");
+        // }else{
+        //     System.out.println("you do not have a fever");
+        // }
+
+
+        // int n=sc.nextInt();
+        // switch(n){
+        //     case 1:System.out.println("Sunday");
+        //     break;
+        //     case 2:System.out.println("Monday");
+        //     break;
+        //     case 3:System.out.println("Tuesday");
+        //     break;
+        //     case 4:System.out.println("Wednesday");
+        //     break;
+        //     case 5:System.out.println("Thursday");
+        //     break;
+        //     case 6:System.out.println("Friday");
+        //     break;
+        //     case 7:System.out.println("Saturday");
+        //     default:System.out.println("number is not between 1-7");
+        // }
+
+
+
+        int a=63,b=36;
+        boolean x=(a<b)?true:false;
+        int y=(a>b)?a:b;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
