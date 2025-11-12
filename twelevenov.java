872e588cce77156aaba68a390ef3a6f1f183ed28
Aditya Pratap Singh <adps.prayag@gmail.com>
2025-11-12 18:08:38 +0530

    This is my first file of java

---
+++ twelevenov.java
@@ -0,0 +1,82 @@
+import java.util.*;
+public class twelevenov {
+    public static void main(String args[]){
+        // Scanner sc=new Scanner(System.in);
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int c=sc.nextInt();
+        // int avg=(a+b+c)/3;
+        // System.out.println(avg);
+
+
+        // Scanner sc=new Scanner(System.in);
+        // int side=sc.nextInt();
+        // int area=side*side;
+        // System.out.println("Area of a square:"+area);
+
+
+        // Scanner sc=new Scanner(System.in);
+        // float pencil=sc.nextFloat()*1.18f;
+        // float pen=sc.nextFloat()*1.18f;
+        // float eraser=sc.nextFloat()*1.18f;
+        // System.out.println(pencil+pen+eraser);
+
+        
+        // byte b=4;
+        // char c='a';
+        // short s=512;
+        // int i=1000;
+        // float f=3.14f;
+        // double d=99.9954;
+
+        // double result=(f*d)+ (i%c)-(d*s);
+        // System.out.println(result);
+
+
+
+
+        // int $=24;
+        // System.out.println($);
+
+
+        // int x=2,y=5;
+        // int exp1=(x*y/x);
+        // int exp2=(x*(y/x));
+        // System.out.println(exp1+",");
+        // System.out.println(exp2);
+
+  
+
+
+        // int x=200,y=50,z=100;
+        // if(x>y && y>z){
+        //     System.out.println("Hello");
+        // }
+        // if(z>y && z<x){
+        //     System.out.println("Java");
+        // }if((y+200)<x&&(y+150)<z){
+        //     System.out.println("Hello java");
+        // }
+
+
+        // int x,y,z;
+        // x=y=z=2;
+        // x+=y;
+        // y-=z;
+        // z/=(x+y);
+        // System.out.print(x+" "+y+" "+z);
+
+
+        // int x=9,y=12;
+        // int a=2,b=4,c=6;
+        // int exp=4/3*(x+34)+9*(a+b*c)+(3+y*(2+a))/(a+b*y);
+        // System.out.println(exp);
+
+
+        int x=10,y=5;
+        int exp1=(y*(x/y + x/y));
+        int exp2=(y*x/y+y*x/y);
+        System.out.println(exp1);
+        System.out.println(exp2);
+    }
+}
