a6d8c7cf7b6021329a2e18cf94b68bdf3fc1871e
Aditya Pratap Singh <adps.prayag@gmail.com>
2025-11-16 20:13:41 +0530

    new functions and methods

--- functionMethods.java
+++ functionMethods.java
@@ -68,18 +68,106 @@ public class functionMethods {
     // }
 
 
-    public static boolean isPrime(int n){
-        if(n==2){
-            return true;
+    // public static boolean isPrime(int n){
+    //     if(n==2){
+    //         return true;
+    //     }
+    //     for(int i=2;i<=n-1;i++){
+    //         if(n%i==0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    
+    // public static boolean isPrime(int n){
+    //     if(n==2){
+    //         return true;
+    //     }
+    //     for(int i=2;i<=Math.sqrt(n);i++){
+    //         if(n%i==0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // public static void primesInRange(int n){
+    //     for(int i=2;i<=n;i++){
+    //         if(isPrime(i)){//true
+    //             System.out.print(i+" ");
+    //         }
+    //     }
+    // }
+
+
+    // public static void binaryToDecimal(int n){
+    //     int pow=0;
+    //     int dec=0;
+    //     while(n>0){
+    //         int lastDigit=n%10;
+    //         dec=dec+(int)(lastDigit*Math.pow(2,pow));
+    //         pow++;
+    //         n=n/10;
+    //     }
+    //     System.out.println(dec);
+    // }
+
+
+    // public static void decimalToBinary(int n){
+    //     int pow=0;
+    //     int bin=0;
+    //     while(n>0){
+    //         int rem=n%2;
+    //         bin=bin+(rem*(int)Math.pow(10,pow));
+    //         pow++;
+    //         n=n/2;
+    //     }
+    //     System.out.println(bin);
+    // }
+
+    //FUNCTIONS AND METHODS:PRATICE QUESTION
+    // public static void getAvg(int a,int b,int c){
+    //     int sum=a+b+c;
+    //     System.out.println(sum);
+    // }
+       
+    // public static boolean isEven(int n){
+    //     if(n%2==0){
+    //         return true;
+    //     }else{
+    //         return false;
+    //     }
+    // }
+
+
+    // public static void isPallindrome(int n){
+    //     int x=n;
+    //     int rev=0;
+    //     while(n>0){
+    //         int lastDigit=n%10;
+    //         rev=rev*10+lastDigit;
+    //         n=n/10;
+    //     }
+    //     if(x==rev){
+    //         System.out.println("Pallindrome number");
+    //     }else{
+    //         System.out.println("Not a Pallindrome number");
+    //     }
+    // }
+
+    
+    public static void sum(int n){
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=rem;
+            n/=10;
         }
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
+        System.out.println(sum);
     }
-    public static void main(String[] args) {
-       System.out.println(isPrime(13));
+      public static void main(String[] args) {
+      sum(4444);
     }
 }
