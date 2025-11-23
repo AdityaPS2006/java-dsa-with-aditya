be279b5bf85d007b997e315917f2a42550422a37
Aditya Pratap Singh <adps.prayag@gmail.com>
2025-11-23 18:25:00 +0530

    Patterns Advanced Question

---
+++ pattern2.java
@@ -0,0 +1,149 @@
+public class pattern2 {
+    public static void hollow_pattern(int totalRow,int totalCol){
+        for(int i=1;i<=totalRow;i++){
+            for(int j=1;j<=totalCol;j++){
+                if(i==1 || j==1 ||i==totalRow ||j==totalCol){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void inverted_rotated_half_pyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void inverted_half_pyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    
+    public static void floyds_triangle(int n){
+        int k=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void zero_one_triangle(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print(1);
+                }else{
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    
+    public static void butterfly_pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solid_rhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollow_rhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(j==1||j==n||i==1||i==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void diamond_pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+   public static void main(String[] args) {
+    //    hollow_pattern(5,5);
+    // inverted_rotated_half_pyramid(4);
+    // inverted_half_pyramid(5);
+    // floyds_triangle(4);
+    // zero_one_triangle(5);
+    // butterfly_pattern(4);
+    // solid_rhombus(5);
+    // hollow_rhombus(5);
+    diamond_pattern(4);
+   }
+}
